a2d70acd5e9e6dee37c9b48341159c2c5bfa38a2
sandy <pounoussamy.sandy@gmail.com>
2021-08-12 16:30:44 +0100

    Added extra features during test: handling days of week (as string or integers) and days of months

--- lib/cron_parser.rb
+++ lib/cron_parser.rb
@@ -1,84 +1,67 @@
+def field_parser(cron_field, start, limit, mapping)
+
+  if !mapping.nil?
+    mapping.each_key do |day|
+      if cron_field.include?(day.to_s)
+        cron_field.gsub!(day.to_s, mapping[day].to_s)
+      end
+    end
+  end
+
+  if cron_field == "*"
+    result = Array(start..limit)
+    result.map! { |time| time.to_s }
+  elsif cron_field == "0"
+    result = ["0"]
+  elsif cron_field.include?("/")
+    if cron_field.split("/")[0] == "*"
+      start_at = start
+    else
+      start_at = cron_field.split("/")[0].to_i
+    end
+    result = []
+    repeat_every = cron_field.split("/")[1].to_i
+    while start_at <= limit do
+      result.push(start_at)
+      start_at += repeat_every
+    end
+    result.map! { |time| time.to_s }
+  elsif cron_field.include?(",")
+    result = cron_field.split(",")
+  elsif cron_field.include?("-")
+    result = Array(cron_field.split("-")[0]..cron_field.split("-")[1])
+  else
+    result = [cron_field]
+  end
+end
+
+LIMIT_VALUES = [59, 23, 31, 12, 7]
+WEEKDAY = {
+  "MON": 1, 
+  "TUE": 2,
+  "WED": 3,
+  "THU": 4,
+  "FRI": 5,
+  "SAT": 6,
+  "SUN": 7
+}
 
 def cron_expression_parse(cron_expression)
   cron_array = cron_expression.split(" ")
 
-  if cron_array[0] == "*"
-    minutes = Array(0..59)
-    minutes.map! { |time| time.to_s }
-  elsif cron_array[0] == "0"
-    minutes = ["0"]
-  elsif cron_array[0].include?("/")
-    if cron_array[0].split("/")[0] == "*"
-      start_at = 0
-    else
-      start_at = cron_array[0].split("/")[0].to_i
-    end
-    minutes = []
-    repeat_every = cron_array[0].split("/")[1].to_i
-    while start_at <= 59 do
-      minutes.push(start_at)
-      start_at += repeat_every
-    end
-    minutes.map! { |time| time.to_s }
-  elsif cron_array[0].include?(",")
-    minutes = cron_array[0].split(",")
-  elsif cron_array[0].include?("-")
-    minutes = Array(cron_array[0].split("-")[0]..cron_array[0].split("-")[1])
-  else
-    minutes = [cron_array[0]]
-  end
-
-  if cron_array[1] == "*"
-    hour = Array(0..23)
-    hour.map! { |time| time.to_s }
-  elsif cron_array[1] == "0"
-    hour = ["0"]
-  elsif cron_array[1].include?("/")
-    if cron_array[1].split("/")[0] == "*"
-      start_at = 0
-    else
-      start_at = cron_array[1].split("/")[0].to_i
-    end
-    hour = []
-    repeat_every = cron_array[1].split("/")[1].to_i
-    while start_at <= 23 do
-      hour.push(start_at)
-      start_at += repeat_every
-    end
-    hour.map! { |time| time.to_s }
-  elsif cron_array[1].include?(",")
-    hour = cron_array[1].split(",")
-  elsif cron_array[1].include?("-")
-    hour = Array(cron_array[1].split("-")[0]..cron_array[1].split("-")[1])
-  else
-    hour = [cron_array[1]]
-  end
-
-  if cron_array[3] == "*"
-    months = Array(1..12)
-    months.map! { |month| month.to_s }
-  elsif cron_array[3].include?("/")
-    start_at = cron_array[3].split("/")[0].to_i
-    repeat_every = cron_array[3].split("/")[1].to_i
-    months = []
-    while start_at <= 12 do
-      months.push(start_at)
-      start_at += repeat_every
-    end
-    months.map! { |month| month.to_s }
-  elsif cron_array[3].include?(",")
-    months = cron_array[3].split(",")
-  elsif cron_array[3].include?("-")
-    months = Array(cron_array[3].split("-")[0]..cron_array[3].split("-")[1])
-  else
-    months = [cron_array[3]]
-  end
+  minutes = field_parser(cron_array[0], 0, LIMIT_VALUES[0], nil)
+  hour = field_parser(cron_array[1], 0, LIMIT_VALUES[1], nil)
+  day_of_month = field_parser(cron_array[2], 1, LIMIT_VALUES[2], nil)
+  months = field_parser(cron_array[3], 1, LIMIT_VALUES[3], nil)
+  day_of_week = field_parser(cron_array[4], 1, LIMIT_VALUES[4], WEEKDAY)
 
 
   puts "minute        " + minutes.join(" ")
   puts "hour          " + hour.join(" ")
-  puts "day of month  "
+  puts "day of month  " + day_of_month.join(" ")
   puts "month         " + months.join(" ")
-  puts "day of week   "
+  puts "day of week   " + day_of_week.join(" ")
   puts "command       " + cron_array[5]
 end
+
+
